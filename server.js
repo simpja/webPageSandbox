47976ab425cc3f7428a5b5af6b21543635e047fa
simpja <simenpjaat@gmail.com>
2020-06-26 10:22:20 +0200

    made proper routing with express server

--- server.js
+++ server.js
@@ -1,24 +1,34 @@
-var http = require("http");
-const fs = require("fs");
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
 const port = 3000;
+const path = require("path");
+const router = express.Router();
+// const routes = require("./routes/index");
 
-const server = http.createServer(function(req, res) {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  fs.readFile("./index.html", function(error, data) {
-    if (error) {
-      res.writeHead(404);
-      res.write("Error: File not found");
-    } else {
-      res.write(data);
-    }
-    res.end();
-  });
+// For our home screen we add a route to the empty directory
+// the route simply serves the static html file in the home folder.
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/home/index.html"));
 });
+/*
+...And in order for this static HTML-file to reach its CSS- and js files (static assets), 
+we need to serve the home folder directory as static assets using app.use('path')
+However, this approach puts all assets in the same root folder
+Better then to add a virtual server path like this: app.use('virtual server path', 'local asset path');
+In that way, we can categorize all our assets in folders
+*/
+app.use("/home", express.static(__dirname + "/home/"));
 
-server.listen(port, function(error) {
-  if (error) {
-    console.log("Something went wrong!", error);
-  } else {
-    console.log("server is listening on port " + port);
-  }
+// Following the same approach as our home screen, we can serve our routes and their static assets the same way
+router.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname + "/about/index.html"));
+});
+app.use("/about", express.static(__dirname + "/about/"));
+app.use("/assets", express.static(__dirname + "/assets/"));
+
+app.use("/", router);
+
+http.listen(port, function () {
+  console.log(`listening on: ${port}`);
 });
